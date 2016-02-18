ab8d6327bd77d632f72d51364686bf7b964cbea6
Braxton Christensen <braxton.christensen@gmail.com>
2016-02-17 19:46:31 -0700

    about to refactor to es6

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -5,25 +5,39 @@ var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
 var ReactFireMixin = require('reactfire');
 var Firebase = require('firebase');
+var helpers = require('../utils/helpers');
 
 var Profile = React.createClass({
   mixins: [ReactFireMixin],
   getInitialState: function(){
     return {
       notes: [1,2,3],
-      bio: {
-        name: 'Braxton'
-      },
-      repos: ['a', 'b', 'c']
+      bio: {},
+      repos: []
     }
   },
   componentDidMount: function(){
     this.ref = new Firebase('https://githubnotetakerbmc.firebaseio.com/');
-    var childRef = this.ref.child(this.props.params.username);
-    this.bindAsArray(childRef, 'notes');
+    this.init(this.props.params.username);
+  },
+  componentWillReceiveProps: function(nextProps) {
+    this.unbind('notes')
+    this.init(nextProps.params.username);
   },
   componentWillUnmount: function(){
     this.unbind('notes');
+  },
+  init: function(username) {
+    var childRef = this.ref.child(username);
+    this.bindAsArray(childRef, 'notes');
+
+    helpers.getGithubInfo(username)
+      .then(function(data) {
+        this.setState({
+          bio: data.bio,
+          repos: data.repos
+        })
+      }.bind(this))
   },
   handleAddNote: function(newNote) {
     this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote)
